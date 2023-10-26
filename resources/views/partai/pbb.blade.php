4dbd1eb66e3cee4f99043d1bd842cb8889fe7362
bluepeailla <106515674+bluepeailla@users.noreply.github.com>
2023-10-27 06:39:15 +0700

    Update pbb.blade.php -illa

--- resources/views/partai/pbb.blade.php
+++ resources/views/partai/pbb.blade.php
@@ -1,54 +1,69 @@
-@extends('layouts.app')
-
-@section('content')
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ URL::asset('css/customize_party.css') }}">
-    <title></title>
-    <style>
-        .container {
-            text-align: center;
-            margin: auto;
-            max-width: 600px;
-        }
-        .logo {
-            max-width: 200px;
-            margin-bottom: 20px;
-        }
-        .left, .right {
-            display: inline-block;
-            width: 100%;
-            text-align: center;
-        }
-    </style>
+    <link rel="icon" href="{{ asset('images/icon.png') }}" type="image">
+    <title>kepoin partai | PBB</title>
 </head>
 <body>
-    <div class="container d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
-        <img src="{{ asset('images/image_13.png') }}" alt="PBB" style="max-width: 50%; max-height: 50%;">
-        <h1>pbb</h1>
-        <h4>Ketua umum: {{ $profile_pbb -> ketua_umum }}</h4>
+@extends('layouts.app')
+
+<section>
+    <div class="container" style="padding-top: 120px;">
+        <img class = "logo-partai"src="{{ asset('images/image_13.png') }}" alt="logo partai" >
+        <h1 style="margin-bottom: 20px; color: #5038bc; font-weight: bold">Partai Bulan Bintang .</h1>
+
+        <div class="card" style=" border-radius: 20px">
+        <h3>Ketua umum: {{ $profile_pbb -> ketua_umum }}</h3>
+        </div>
+
+        <div class="card" style=" border-radius: 20px">
+        <h3 style="color: #5038bc;font-weight: medium">Ideologi</h3>
         <p>Ideologi: Demokrasi Islam, Nasionalisme religius</p>
-        <br></br>
-        <div class="left">
-            <h3>Kasus Korupsi</h3>
+        </div>
+
+        <div class="card" style=" border-radius: 20px">
+            <h3 style="color: #5038bc;font-weight: medium">Kasus Korupsi</h3>
             <p>Total Kasus: {{ $profile_pbb -> jumlah_kasus_korupsi }}</p>
             <p>Total Kerugian Negara: {{ $profile_pbb -> nominal_kasus_korupsi }} Miliar</p>
         </div>
         
-        <div class="right">
-            <h3>Suap & Gratifikasi</h3>
+        <div class="card" style=" border-radius: 20px">
+            <h3 style="color: #5038bc; font-weight: medium">Suap & Gratifikasi</h3>
             <p>Total Kasus: {{ $profile_pbb -> jumlah_kasus_suap_gratifikasi }}</p>
             <p>Total Nominal: {{ $profile_pbb -> nominal_suap_gratifikasi }} Miliar</p>
-            <h3>Fakta menarik:</h3>
-            <p>Artis eksentrik, Aldi Taher sempat menyalonkan diri menjadi perwakilan di Dewan Perwakilan Rakyat (DPR) untuk PBB dan Perindo. Wawancaranya di TvOne menjadi meme ketika dia memberikan jawaban-jawaban ‘unik’ saat ditanya tentang isu-isu penting.</p>
+        </div>
+
+        <div class="card" style=" border-radius: 20px">
+        <h3 style="color: #5038bc;font-weight: medium">Fakta Menarik:</h3> 
+        <p>Artis eksentrik, Aldi Taher sempat menyalonkan diri menjadi perwakilan di Dewan Perwakilan Rakyat (DPR) untuk PBB dan Perindo. Wawancaranya di TvOne menjadi meme ketika dia memberikan jawaban-jawaban ‘unik’ saat ditanya tentang isu-isu penting.</p>
         </div>
     </div>
+</section>
+
+<section>
+<section>
+    <div class="feedback">
+        <h1 class="mt-3 text-typing-animation">Informasi yang ditampilkan tidak tepat? Yuk kirim <span style="color: #5038bc; font-weight: medium">feedbackmu!</span></h1>
+        <a href="{{ route('feedback') }}" id="feedback-button" class="button-86 mt-3">Feedback</a>
+    </div>
+</section>
+
+
+<script>
+    // JavaScript for hover effect on the feedback button
+    const feedbackButton = document.getElementById('feedback-button');
+
+    feedbackButton.addEventListener('mouseover', function () {
+        feedbackButton.style.backgroundColor = '#422c9d'; // Darker purple on hover
+    });
+
+    feedbackButton.addEventListener('mouseout', function () {
+        feedbackButton.style.backgroundColor = '#5038bc'; // Purple color on mouseout
+    });
+</script>
+</section>
 </body>
 </html>
-<div class="container d-flex flex-column justify-content-center align-items-center" style="height: 100vh;">
-    <p class="mt-3">Informasi yang ditampilkan tidak tepat? Yuk kirim feedbackmu!</p>
-    <a href="{{ route('feedback') }}" class="btn btn-primary mt-3">Feedback</a>
-</div>
-@endsection
