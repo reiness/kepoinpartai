a3ead3a437acb354a91d785b649f7037ec567ad3
ridhopandhu <rdhpndh@gmail.com>
2023-10-26 10:16:54 +0700

    adding all partai minus the inside

--- resources/views/partai_all.blade.php
+++ resources/views/partai_all.blade.php
@@ -3,34 +3,117 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-2">
             <div class="partai-item">
                 <img src="{{ asset('images/image_1.png') }}" alt="Partai 1" style="max-width: 100%; height: auto;">
-                <button class="btn btn-primary mt-3">Lihat Profil</button>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="partai-item">
-                <img src="{{ asset('images/image_2.png') }}" alt="Partai 2" style="max-width: 100%; height: auto;">
-                <button class="btn btn-primary mt-3">Lihat Profil</button>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="partai-item">
-                <div class="image-container">
-                    <img src="{{ asset('images/image_3.png') }}" alt="Partai 3" class="img-fluid">
-                </div>
                 <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
             </div>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-2">
             <div class="partai-item">
-                <img src="{{ asset('images/image_4.png') }}" alt="Partai 4" style="max-width: 100%; height: auto;">
-                <button class="btn btn-primary mt-3">Lihat Profil</button>
+                <img src="{{ asset('images/image_2.png') }}" alt="Partai 2" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
             </div>
         </div>
-        
-        
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_3.png') }}" alt="Partai 3" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_4.png') }}" alt="Partai 4" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_5.png') }}" alt="Partai 5" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_6.png') }}" alt="Partai 6" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
     </div>
+    <div class="row">
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_7.png') }}" alt="Partai 7" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_8.png') }}" alt="Partai 8" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_9.png') }}" alt="Partai 9" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_10.png') }}" alt="Partai 10" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_11.png') }}" alt="Partai 11" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_12.png') }}" alt="Partai 12" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_13.png') }}" alt="Partai 13" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_14.png') }}" alt="Partai 14" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_15.png') }}" alt="Partai 15" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_16.png') }}" alt="Partai 16" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_17.png') }}" alt="Partai 17" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <div class="partai-item">
+                <img src="{{ asset('images/image_18.png') }}" alt="Partai 18" style="max-width: 100%; height: auto;">
+                <a href="{{ route('partai.pdip') }}" class="btn btn-primary mt-3">Lihat Profil</a>
+            </div>
+        </div>
+    </div>
+    
 </div>
 @endsection
