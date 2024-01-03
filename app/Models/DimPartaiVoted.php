d6b8f3e94a9e720263ea0498069791d8dcdc6ab0
Michael A. Herryanto <141414076+michaelah0@users.noreply.github.com>
2024-01-03 20:16:40 +0700

    DimPartaiVoted.php
    Dim Partai Voted

---
+++ app/Models/DimPartaiVoted.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DimAdmin extends Model
+{
+    protected $connection = 'secondary_mysql'
+    protected $fillable = [
+        'sk_vote',
+        'nama_partai',
+        'username'
+    ];
+
+    // Define the one-to-many relationship with UserVote table
+    public function user_partai_vote()
+    {
+        return $this->hasMany(FactVotes::class, 'sk_vote', 'sk_vote');
+    }
+}
