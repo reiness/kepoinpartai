1cf5081e649a0c2eede124d5cab5075d0a5f06ac
Michael A. Herryanto <141414076+michaelah0@users.noreply.github.com>
2024-01-03 20:16:15 +0700

    DimAdmin.php
    dim admin done

---
+++ app/Models/DimAdmin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DimAdmin extends Model
+{
+    protected $connection = 'secondary_mysql'
+    protected $fillable = [
+        'sk_admin',
+        'is_admin',
+        'username'
+    ];
+
+    // Define the one-to-one relationship with UserVote table
+    public function user_admin(): HasOne
+    {
+        return $this->hasOne(FactVotes::class, 'sk_admin', 'sk_admin');
+    }
+}
